8bafbad1a5afe3f0d1b4810dd033723fa1e8d4c4
Sesh <sergeimorozov212001@gmail.com>
2025-12-18 12:52:31 +0300

    fix(form)

--- src/components/ExcursionComponent/ExcursionComponent.tsx
+++ src/components/ExcursionComponent/ExcursionComponent.tsx
@@ -1,25 +1,46 @@
 import styles from './ExcursionComponent.module.css';
+import {useEffect, useLayoutEffect, useRef} from "react";
 
 export const ExcursionComponent = () => {
+  const formContainerRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+
+    const fixedScriptCode = `(function(w,d,u){
+      var s=d.createElement('script');
+      s.async=true;
+      s.src=u+'?'+(Date.now()/180000|0);
+      var h=d.getElementById('bitrix-form-container');
+      h.parentNode.insertBefore(s,h);
+    })(window,document,'https://cdn-ru.bitrix24.ru/b28923876/crm/form/loader_149.js');`;
+
+    // Создаем и добавляем стили
+    const style = document.createElement('style');
+    style.innerHTML = `.b24-form-control-label,.b24-form-control-container { text-align: start !important; }`;
+    document.head.appendChild(style);
+
+
+
+    // Добавляем скрипт
+    const script = document.createElement('script');
+    script.setAttribute('data-b24-form', 'inline/149/p7q2aq');
+    script.setAttribute('data-skip-moving', 'true');
+    script.id = 'bitrix-form-container';
+    script.innerHTML = fixedScriptCode;
+    formContainerRef.current?.appendChild(script);
+
+    // Очистка
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
 
   return (
     <div className={styles.main}>
-      <div className={styles.description_section}>
-        <div className={styles.content}>
-          <form className={styles.form}>
-            <div className={styles.form_header}>Запись на экскурсию в коливинг</div>
-            <hr/>
-            <div className={styles.form_body}>
-              <input className={styles.form_input} placeholder={'Имя'} type='text'/>
-              <input className={styles.form_input} placeholder={'Фамилия'} type='text' />
-              <input className={styles.form_input} placeholder={'E-mail'} type='email'/>
-              <button className={styles.form_button} onClick={() => {}} type={'button'} >Записаться на экскурсию</button>
-            </div>
-          </form>
-        </div>
+      <div className={styles.description_section} ref={formContainerRef}>
+        <div ref={formContainerRef}></div>
       </div>
-
     </div>
-  )
+  );
 
 }
